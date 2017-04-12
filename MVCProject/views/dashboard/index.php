b760cec606cc4b65a25817ed9e0c3eaf474f6458
ShaiEli1991 <shaiel@ac.sce.ac.il>
2017-04-12 16:16:19 +0300

    Add files via upload

---
+++ MVCProject/views/dashboard/index.php
@@ -0,0 +1,13 @@
+Dashboard... Logged in only..
+<br />
+
+<form action="<?php echo URL;?>weather/index/">
+    <input type="submit" value="Weather" />
+</form>
+<form action="<?php echo URL;?>calculator/index/">
+    <input type="submit" value="Calculator" />
+</form>
+<br />
+<div id="listInserts">
+    
+</div>
