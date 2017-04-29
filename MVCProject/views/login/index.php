a32cb05ae76e9ae954d9759fed5a60d68a55ab0b
maorisr <maoris@ac.sce.ac.il>
2017-04-30 00:23:27 +0300

    Delete index.php

--- MVCProject/views/login/index.php
+++
@@ -1,9 +0,0 @@
-<center>
-<h3>Login page:</h3>
-
-<form action="login/run" method="post">
-	<label>Username</label><input type="text" name="username" /><br />
-	<label>Password</label><input type="password" name="password" /><br />
-	<label></label><input type="submit" />
-</form>
-</center>
