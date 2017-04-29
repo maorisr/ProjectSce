ff6adc704cf5c1ece639ee7217e0608b628d7844
maorisr <maoris@ac.sce.ac.il>
2017-04-30 00:22:22 +0300

    Delete index.php

--- MVCProject/views/weather/index.php
+++
@@ -1,6 +0,0 @@
-<center><!--submit this form to 'weather.PHP'-->
-<form action="weather/run/" method="get">
-    City: <input type="text" name="city" placeholder="city"/><br />
-    <input type="submit" name="submit" value="Submit" />
-</form>
-</center>
